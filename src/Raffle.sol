dd84902e170800013b6e70e88d8db79df942bc0f
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-05-22 16:03:08 -0700

    Implement pickWinner function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -12,14 +12,19 @@ contract Raffle {
     error Raffle_NotEnoughEthSent();
 
     uint256 private immutable i_entranceFee;
+    /** @dev duration of the lottery in seconds */
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
     /** ================= Events ================= */
     event EnteredRaffle(address indexed player);
 
     /** ================= Functions ================= */
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
@@ -27,7 +32,11 @@ contract Raffle {
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
     }
-    function pickWinner() public {}
+    function pickWinner() external {
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert();
+        }
+    }
 
     /** ================= Getters ================= */
     function getEntranceFee() external view returns(uint256) {
