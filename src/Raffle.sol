ce606300975b3a0717eb999257aa5e1da750ca20
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-05-23 09:39:37 -0700

    Finish picking up a random number & winner

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -13,8 +13,14 @@ import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/vrf/inter
 import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 contract Raffle is VRFConsumerBaseV2{
+    /** ================= Errors ================= */
     error Raffle__NotEnoughEthSent();
     error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
+
+    /** ================= Type Declarations ================= */
+    /** @dev each field in enum can be converted to interge => for example: Open would be 0, calculating would be 1 */
+    enum RaffleState {OPEN, CALCULATING}
 
     /** ================= State Variables ================= */
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
@@ -29,19 +35,14 @@ contract Raffle is VRFConsumerBaseV2{
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
     address private s_recentWinner;
+    RaffleState private s_raffleState;
 
     /** ================= Events ================= */
     event EnteredRaffle(address indexed player);
+    event PickedWinner(address indexed winner);
 
     /** ================= Functions ================= */
-    constructor(
-        uint256 entranceFee,
-        uint256 interval,
-        address vrfCoordinator,
-        bytes32 gasLane,
-        uint64 subscriptionId,
-        uint32 callbackGasLimit
-    ) VRFConsumerBaseV2(vrfCoordinator) {
+    constructor(uint256 entranceFee,uint256 interval,address vrfCoordinator,bytes32 gasLane,uint64 subscriptionId,uint32 callbackGasLimit) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
@@ -49,24 +50,33 @@ contract Raffle is VRFConsumerBaseV2{
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
         s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
     }
 
+    // ✅ CEI Verified Function
     function enterRaffle() external payable {
-        if (msg.value < i_entranceFee) {
-            revert Raffle__NotEnoughEthSent();
-        }
+        // Checks
+        if (msg.value < i_entranceFee) { revert Raffle__NotEnoughEthSent(); }
+        if (s_raffleState != RaffleState.OPEN) { revert Raffle__RaffleNotOpen(); }
+        // Effects
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
     }
+
+    // ✅ CEI Verified Function
     function pickWinner() external {
+        // Checks
         if (block.timestamp - s_lastTimeStamp < i_interval) {
             revert();
         }
+        // Effects (Our own contract)
+        s_raffleState = RaffleState.CALCULATING;
         /**
             @dev Here's what gonna happen: 
             @dev We're gonna make a request to the chainlink node to give us a random number (requestRandomWords);
         */
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+        // Interactions (Other contracts)
+        i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
             REQUEST_CONFIRMATIONS,
@@ -83,13 +93,17 @@ contract Raffle is VRFConsumerBaseV2{
         */
     }
 
-    function fulfillRandomWords(
-        uint256 requestId,
-        uint256[] memory randomWords
-    ) internal override {
+    // ✅ CEI Verified Function
+    function fulfillRandomWords(uint256,uint256[] memory randomWords) internal override {
+        // Effects (Our own contract)
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable winner = s_players[indexOfWinner];
         s_recentWinner = winner;
+        s_raffleState = RaffleState.CALCULATING;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit PickedWinner(winner);
+        // Interactions (Other contracts)
         (bool success,) = winner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
